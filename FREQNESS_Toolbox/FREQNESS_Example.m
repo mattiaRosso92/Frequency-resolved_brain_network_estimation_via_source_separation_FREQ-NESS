398d47dc9fa2a0c4d8604124099afbd1a5c851ab
Mattia Rosso <mattia.red92@gmail.com>
2025-04-21 15:07:11 +0200

    Update FREQNESS_Example.m

--- FREQNESS_Toolbox/FREQNESS_Example.m
+++ FREQNESS_Toolbox/FREQNESS_Example.m
@@ -2,9 +2,9 @@
 %  FREQNESS: EXAMPLE SCRIPT FOR BRAIN NETWORK ESTIMATION
 %
 %  Please cite the first FREQNESS paper:
-%  M. Rosso, G. Fernandez-Rubio, P. E. Keller, E. Brattico, P. Vuust, M. L. Kringelbach, L. Bonetti.
-%  FREQ-NESS Reveals the Dynamic Reconfiguration of Frequency-Resolved Brain Networks During Auditory Stimulation.
-%  Adv. Sci. 2025, 2413195.
+%  Rosso, M., Fernández‐Rubio, G., Keller, P. E., Brattico, E., Vuust, P., Kringelbach, M. L., & Bonetti, L. (2025). 
+%  FREQ‐NESS Reveals the Dynamic Reconfiguration of Frequency‐Resolved Brain Networks During Auditory Stimulation. 
+%  Advanced Science, 2413195.
 %  https://doi.org/10.1002/advs.202413195
 %
 %
